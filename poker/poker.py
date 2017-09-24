a82fb9c41307d2ccc696de3a3e92a6ef460e958a
wangyining18 <lynnw28@gmail.com>
2017-09-24 01:07:55 -0400

    upload right files

--- poker/poker.py
+++ poker/poker.py
@@ -3,7 +3,7 @@ from deuces import Card
 from deuces import Evaluator
 from deuces import Deck
 import random
-
+import copy
 ################ README:
 ################ Only change the lines that explicitly tell you than can be changed, otherwise your final program will not work with our grading process
 
@@ -13,16 +13,69 @@ class Player:
 	def __init__(self, startMoney):
 		self.money = startMoney
 	def returnBestHand(self, board): #CHANGE THIS FUNCTION TO RETURN THE BEST HAND WHEN ASKED AT THE RIVER
-		return board
+		return evaluator.evaluate(board, self.curHand)
 	def callAI(self, state): #CHANGE THIS FUNCTION. NEVER REFERENCE OTHER PLAYER'S CARDS. You are not allowed to cheat! (obviously) e.g. If we see curState.players[x].curHand, that's unacceptable.
-		raiseAmount = random.randint(0, 100)
-		maxbid = max(raiseAmount, random.randint(0, 100))
-		if maxbid > self.money: #do not remove
-			maxbid = self.money
-		if raiseAmount > self.money: #do not remove
-			raiseAmount = self.money
-		possibleActions = ["check", ["raise", raiseAmount]] #can only check or raise, since only one action is processed. Fold if max bid is lower than the biggest raise.
-		return [ possibleActions[random.randint(0,len(possibleActions) - 1)], maxbid ]
+		MAX_SCORE= 7462
+		score = 0
+		# we evaluate the score of the current situation and based on the estimated score, pick an acion
+		# We randomly generate the board N times and get the average score
+		N = 5
+		i = 0
+
+		if curState.curStage == "river":
+			score = score + evaluator.evaluate(curState.board, self.curHand)
+		else:
+			while i < N:
+				my_deck = Deck()
+				my_board = my_deck.draw(0)
+
+				if curState.curStage == "preflop":
+					exclude = copy.deepcopy(self.curHand)
+					my_board = self.draw(5, exclude, my_deck)
+
+				elif curState.curStage == "flop":
+					exclude = copy.deepcopy(self.curHand) + copy.deepcopy(curState.board)
+					my_board = curState.board + self.draw(2, exclude, my_deck)
+
+				elif curState.curStage == "turn":
+					exclude = copy.deepcopy(self.curHand) + copy.deepcopy(curState.board)
+					my_board = curState.board +  self.draw(1, exclude, my_deck)
+
+
+				score = score + evaluator.evaluate(self.curHand, my_board)
+				i = i + 1
+			score = score / N
+		#raiseAmount = random.randint(0, 100)
+		#maxbid = max(raiseAmount, random.randint(0, 100))
+		#if maxbid > self.money: #do not remove
+		#	maxbid = self.money
+		#if raiseAmount > self.money: #do not remove
+		#	raiseAmount = self.money
+		#possibleActions = ["check", ["raise", raiseAmount]] #can only check or raise, since only one action is processed. Fold if max bid is lower than the biggest raise.
+
+		#There are 7462 scores in total (smaller is better). We choose an action based on the estimated score
+		if score > MAX_SCORE * 9 / 10: # the score is too high, choose fold
+			raiseAmount = 0
+			action = ["raise", 0]
+			maxbid = 0           # maxbid = 0, means fold
+		elif score > MAX_SCORE / 2:
+			action = ["check"]
+			maxbid = 0
+		else:
+			raiseAmount = random.randint(0, self.money * (1 - score / MAX_SCORE) / 2)
+			maxbid = max(raiseAmount, random.randint(0, self.money * (1 - score / MAX_SCORE)) )
+			action = ["raise",raiseAmount ]
+		return [ action, maxbid ]
+	def draw(self, num, exclude, my_deck):
+		my_board=[]
+		while len(my_board) < num:
+			my_deck.shuffle()
+			d = my_deck.draw(1)
+			if d not in exclude:
+				my_board.append(d)
+				exclude.append(d)
+
+		return my_board
 
 #holds state, you'll need to reference this in callAI
 class State:
@@ -99,6 +152,7 @@ while len(players) > 1:
 	#go through betting stages
 	for stage in range(0, len(curState.stages)):
 		curState.curStage = curState.stages[stage]
+		print("stage: " + curState.curStage )
 		if curState.curStage == "flop":
 			deal(3)
 		elif curState.curStage == "turn":
@@ -130,4 +184,6 @@ while len(players) > 1:
 	for player in curState.players:
 		if player.money == 0:
 			curState.players.remove(player)
+
+
 print(str(curState.players[0]) + " has won!")
